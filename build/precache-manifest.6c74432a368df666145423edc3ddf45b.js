fe7a86db10f0bce74be6561a93aa4424ab3cd702
Andrei Cinc <andrei.cinc9@gmail.com>
2020-05-17 19:08:50 +0300

    Crypto app basic labels&functions

---
+++ build/precache-manifest.6c74432a368df666145423edc3ddf45b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0c0f5a16bc01dbb6612241c117b90b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e81575e9022c86ec9a3",
+    "url": "/static/css/main.0690f137.chunk.css"
+  },
+  {
+    "revision": "d6e0b3dce4f017a39267",
+    "url": "/static/js/2.2bdad9de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2bdad9de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e81575e9022c86ec9a3",
+    "url": "/static/js/main.d9f7407d.chunk.js"
+  },
+  {
+    "revision": "6a1ee889cccb0e812e26",
+    "url": "/static/js/runtime-main.8b8a512a.js"
+  },
+  {
+    "revision": "828a11fcbed5f7635a0f5003cdf23d13",
+    "url": "/static/media/background.828a11fc.png"
+  }
+]);
